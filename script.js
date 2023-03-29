3f6529db03ce7db2dc19b49d770d5c12d274a119
Rick van de Sande <ricksand86@hotmail.com>
2023-03-29 16:22:25 +0200

    Mobile navigation done apart from font

---
+++ script.js
@@ -0,0 +1,17 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navBackground = document.querySelector(".mobile-nav-bg-overlay");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visible = primaryNav.getAttribute("data-visible");
+    
+    if (visible === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navBackground.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        primaryNav.setAttribute("data-visible", false);
+        navBackground.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false)
+    }
+});
